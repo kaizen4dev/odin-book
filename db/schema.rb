ed5140c47b35804631ce7212edf323ef3ab390a7
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-20 19:44:56 +0200

    Add user reference for comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_20_173301) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_20_174411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_20_173301) do
     t.string "commantable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -62,6 +64,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_20_173301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "users"
   add_foreign_key "user_followings", "users", column: "follower_id"
   add_foreign_key "user_followings", "users", column: "following_id"
