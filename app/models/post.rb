c6066caf9244e6d6b3680022a52337024cb26522
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-21 13:21:48 +0200

    Add feed_for scope for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commantable
   has_many :likes, as: :likeable
+
+  scope :feed_for, ->(user) { where(user_id: user.following.ids) }
 end
