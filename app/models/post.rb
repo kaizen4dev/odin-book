8cb048e1c6e9f3310dfebfdd3b878a006609a7cb
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-22 19:53:01 +0200

    fix: typo in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, as: :commantable
   has_many :likes, as: :likeable
 
-  valibates :title, presence: true, length: {in: 5..100}
-  validates :body, presence: true, length: {maximum: 10000}
+  validates :title, presence: true, length: { in: 5..100 }
+  validates :body, presence: true, length: { maximum: 10000 }
 
   scope :feed_for, ->(user) { where(user_id: user.following.ids) }
 end
