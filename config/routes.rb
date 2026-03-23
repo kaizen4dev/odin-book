7415f113ff23653d3e106cb2c7e025c5ab23ce15
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-23 14:11:49 +0200

    Add routes for UserFollowings

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
   resources :users, param: :username, only: [ :show, :index ]
+  resources :user_followings, only: [ :create, :destroy ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
