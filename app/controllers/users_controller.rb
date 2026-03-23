f8057d76b9efc031786edd586c70caa5ec279187
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-23 10:35:18 +0200

    Add index action for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by name: params[:username]
   end
+
+  def index
+    @all = User.all
+    @followers = current_user.followers
+    @following = current_user.following
+  end
 end
