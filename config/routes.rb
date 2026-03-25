821db030bc2daa8202e4328e077db5d4b4030732
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-25 15:50:44 +0200

    Exclude index route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts
-  resources :comments
+  resources :comments, except: [ :index ]
   resources :users, param: :username, only: [ :show, :index ]
   resources :user_followings, only: [ :create, :destroy ]
 
