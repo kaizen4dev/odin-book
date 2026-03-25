dc269da4c9f4762c3f3f3c7e261b2fee069e7b0a
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-25 16:58:01 +0200

    Make copy button say 'copied'

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clipboard"
 export default class extends Controller {
-  static values = {text: String}
+  static values = { text: String }
 
   connect() {
   }
 
   copyText() {
     navigator.clipboard.writeText(this.textValue)
+    let text = this.element.textContent
+    this.element.textContent = "Copied"
+    setTimeout(() => { this.element.textContent = text }, 1200)
   }
 }
