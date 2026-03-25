c88524eeb42d2a6857df2b9d45cf63e39650c4f4
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-25 17:09:54 +0200

    Add #liked_by? method for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,8 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 10000 }
 
   scope :feed_for, ->(user) { where(user_id: user.following.ids.push(user.id)).order(created_at: :desc) }
+
+  def liked_by?(user)
+    self.likes.find_by(user: user).nil?
+  end
 end
