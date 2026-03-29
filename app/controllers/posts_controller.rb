4a1ff90f5811f9150cfd27812b0a20c10809601c
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-29 23:33:15 +0300

    Change order of comments for posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,6 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
     @new_comment = @post.comments.new user: current_user
-    @comments = Comment.where(commentable: @post)
+    @comments = Comment.where(commentable: @post).order(created_at: :desc)
   end
 end
