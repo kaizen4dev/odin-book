2f48cdb2d7efdbf82ce0ca80d03163e492e1f493
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-03-29 17:21:23 +0300

    Add ability to create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new comment_params
+
+    if @comment.save
+      redirect_back_or_to :root
+    else
+      redirect_back_or_to :root, notice: @comment.errors
+    end
+  end
+
+  private
+
+  def comment_params
+    params.expect comment: [ :user_id, :body, :commentable_type, :commentable_id ]
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
-    @new_comment = @post.comments.new
+    @new_comment = @post.comments.new user: current_user
     @comments = Comment.where(commentable: @post)
   end
 end
