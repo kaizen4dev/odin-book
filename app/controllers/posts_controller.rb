7236ebd0d4b7faaefbdc4cf7fa39395c778f4ac6
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-04-02 01:02:57 +0300

    Add ability to create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,24 @@ class PostsController < ApplicationController
     @new_comment = @post.comments.new user: current_user
     @comments = Comment.where(commentable: @post).order(created_at: :desc)
   end
+
+  def new
+    @post = Post.new user: current_user
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect post: [ :title, :body, :user_id ]
+  end
 end
