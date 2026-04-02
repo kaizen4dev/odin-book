0d565b095435ddb7ce65b1bdb449c00e35826299
kaizen4dev <186304347+kaizen4dev@users.noreply.github.com>
2026-04-02 18:39:20 +0300

    Enable devise email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   has_many :user_followings_as_follower, foreign_key: :follower_id, class_name: "UserFollowing"
   has_many :user_followings_as_followed, foreign_key: :following_id, class_name: "UserFollowing"
